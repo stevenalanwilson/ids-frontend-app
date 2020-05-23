bec912040ef959183d0c5bb0728ceaaf5a6a68f7
Steven Wilson <steven@dominion79.co.uk>
2020-05-23 18:08:41 +0100

    Add pages
    - Add case study page
    - Add contact  page
    - Add sectors page

---
+++ pages/casestudies.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function services () {
+  return (
+    <div className='container mx-auto'>
+      <Head>
+        <title>IDS Website - Prototype</title>
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+
+      <main>
+        <div className='flex my-4'>
+          <div className='w-full p-2'>
+            <Header />
+          </div>
+        </div>
+
+        <div className='flex my-4'>
+          <div className='w-1/2 p-10'>
+            <p>Case Studies</p>
+          </div>
+        </div>
+
+        <div className='flex my-4'>
+          <div className='w-full'>
+            <Footer />
+          </div>
+        </div>
+      </main>
+
+      <style jsx>{`
+       
+      `}
+      </style>
+
+    </div>
+  )
+}
