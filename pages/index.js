4dea77e8511d3da7193fec65de4381db118478a2
Steven Wilson <steven@dominion79.co.uk>
2020-05-23 13:21:40 +0100

    Refactored home page to use Header component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home () {
   return (
@@ -15,45 +16,9 @@ export default function Home () {
       </Head>
 
       <main>
-
         <div className='flex my-4'>
           <div className='w-full p-2'>
-
-            <header>
-              <div className='flex items-center'>
-                <div>
-                  <img src='ids-logo.svg' alt='Logo' className='h-16' />
-                </div>
-                <div className='ml-6'>
-                  <h1 className='text-l text-gray-400'>IDS Website Prototype</h1>
-                </div>
-              </div>
-
-            </header>
-          </div>
-        </div>
-
-        <div className='flex my-4'>
-          <div className='w-full p-2'>
-            <nav>
-              <ul className='flex'>
-                <li className='mr-3'>
-                  <a className='inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white' href='#'>Home</a>
-                </li>
-                <li className='mr-3'>
-                  <a className='inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3' href='#'>Services</a>
-                </li>
-                <li className='mr-3'>
-                  <a className='inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3' href='#'>Case Studies</a>
-                </li>
-                <li className='mr-3'>
-                  <a className='inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3' href='#'>Contact</a>
-                </li>
-                <li className='ml-auto'>
-                  <a className='inline-block py-1 px-3  text-gray-400 cursor-not-allowed' href='#'>01889 568573</a>
-                </li>
-              </ul>
-            </nav>
+            <Header />
           </div>
         </div>
 
