73b4acbafa6ed03597b7d9771f1af3cfc9949a24
Steven Wilson <steven@dominion79.co.uk>
2020-05-31 07:52:32 +0100

    Refactor Hero component
    - Random generate  image
    - Set consistant height on the image

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,24 @@
 import Link from 'next/link'
 
+const images = [
+  '/tmp-image-00001.jpeg',
+  '/tmp-image-00002.jpeg',
+  '/tmp-image-00003.jpeg',
+  '/tmp-image-00004.jpeg',
+  '/tmp-image-00005.jpeg',
+  '/tmp-image-00006.jpeg',
+  '/tmp-image-00007.jpeg',
+  '/tmp-image-00008.jpeg'
+]
+
+const randomImageGen = quotes => quotes[Math.floor(Math.random() * (quotes.length - 1))]
+
+const randomImage = randomImageGen(images)
+
 function Hero () {
   return (
     <div className='w-full bg-gray-100 rounded overflow-hidden shadow-lg'>
-      <img src='tmp-hero.jpg' className='object-cover h-200 w-full' />
+      <img src={randomImage} className='object-cover h-64 w-full' />
       <div className='px-6 py-4'>
         <div className='font-bold text-xl mb-2'>Latest Case Study: New Homes in Derby</div>
         <p className='text-gray-700 text-base'>
