fe6ddcd07edf1489abdc602084c6cd8cac3a84dc
Steven Wilson <steven@dominion79.co.uk>
2020-06-07 10:35:56 +0100

    Add tags to Card component
    - Add sector tags to Card  component
    - Add service tags to Card component

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import sectorData from '../data/sectors'
+import serviceData from '../data/services'
 
 function Card ({ data }) {
+  const hasSectors = !!data.sectors
+  const hasServices = !!data.services
   return (
     <div className='w-full p-5'>
       <div className='w-full rounded overflow-hidden shadow-lg'>
@@ -12,6 +16,14 @@ function Card ({ data }) {
         <div className='px-6 py-4'>
           <div className='font-bold text-xl mb-2'>{data.name}</div>
           <p className='text-gray-700 text-base'>{data.description}</p>
+        </div>
+        <div class='px-6 py-1'>
+          {hasSectors && data.sectors.map(function (sector) {
+            return <Link href={sectorData.find(s => s.id === sector).link} key={sector}><a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' key={sector}>{sectorData.find(s => s.id === sector).name}</a></Link>
+          })}
+          {hasServices && data.services.map(function (sector) {
+            return <Link href={serviceData.find(s => s.id === sector).link} key={sector}><a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' key={sector}>{serviceData.find(s => s.id === sector).name}</a></Link>
+          })}
         </div>
       </div>
     </div>
