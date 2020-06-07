c3e8daafc6bf8f2f42533568a6fc902d45fa122e
Steven Wilson <steven@dominion79.co.uk>
2020-06-07 11:35:40 +0100

    Refactor contact link into a call to action

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ function Header () {
           </li>
           <li className='mr-3'>
             <Link href='/contact'>
-              <a className='inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3' href='#'>Contact</a>
+              <a className='inline-block border bg-blue-500 border-blue-500 rounded hover:border-blue-500 hover:text-blue-500 text-white hover:bg-gray-200 py-1 px-3' href='#'>How can we help you?</a>
             </Link>
           </li>
           <li className='ml-auto'>
