bf019366c15b9db801c36829e8e3c9fcf5881cfb
Steven Wilson <steven@dominion79.co.uk>
2020-10-18 18:40:35 +0100

    Refactor config file

---
+++ config.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+module.exports = {
+  appVersion: '0.1.0',
+  appName: 'Ideal Design Soloutions frontend application',
+  environment: process.env.NODE_ENV,
+  space: process.env.SPACEID,
+  accessToken: process.env.ACCESSTOKEN
+}
