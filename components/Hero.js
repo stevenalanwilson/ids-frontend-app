6c0564fc0eb9e39a24a9a9ed7b8912f490087f6a
Steven Wilson <steven@dominion79.co.uk>
2020-10-24 17:54:24 +0100

    Add Hero component

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,13 @@
-import Link from 'next/link'
+import CallToActionLink from '../components/CallToActionLink'
 
 import casestudiesData from '../data/casestudies'
+
+const howCanWeHelp = {
+  name: 'How can we help',
+  link: '/contect',
+  size: 'text-2xl'
+
+}
 
 const randomCaseStudyGen = casestudy => casestudy[Math.floor(Math.random() * (casestudy.length - 1))]
 
@@ -8,19 +15,18 @@ const randomCaseStudy = randomCaseStudyGen(casestudiesData)
 
 function Hero () {
   return (
-    <div className='w-full bg-gray-100 rounded overflow-hidden shadow-lg'>
-      <img src={randomCaseStudy.image} className='object-cover h-64 w-full' />
-      <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>Latest Case Study: {randomCaseStudy.name}</div>
-        <p className='text-gray-700 text-base'>
+    <div className='w-full relative overflow-hidden'>
+      <img src={randomCaseStudy.image} className='object-cover w-full h-200 h-600px' />
+      <div className='absolute top-0 left-0 w-full h-600px bg-white bg-opacity-75' />
+
+      <div className='px-6 py-4 absolute left-0 bottom-150px w-3/4'>
+        <h1 className='font-open-sans font-light text-5xl mb-2'>Latest Case Study: {randomCaseStudy.name}</h1>
+        <p className='font-open-sans font-light text-black text-xl mb-4'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
         </p>
-        <Link href={randomCaseStudy.link}>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-5 rounded'>
-             Read more
-          </button>
-        </Link>
+        <CallToActionLink data={howCanWeHelp} />
       </div>
+      <div className='triangle-bottomleft' />
     </div>
   )
 }
