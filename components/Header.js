a91f176277c0cdb2d1554ac88b5c8f9c6db69688
Steven Wilson <steven@dominion79.co.uk>
2020-10-25 04:34:58 +0000

    Styling update to header

--- components/Header.js
+++ components/Header.js
@@ -28,7 +28,7 @@ const menuData = [
 
 function Header () {
   return (
-    <header className='py-4 fixed z-50 top-0 left-0 w-full'>
+    <header className='py-4 fixed z-50 top-0 left-0 w-full bg-white bg-opacity-75'>
       <nav className='container mx-auto px-4'>
         <ul className='flex items-center'>
           <li className='mr-6'>
