5b80c61ddc210a4af217807dccffa97207d152f0
Steven Wilson <steven@dominion79.co.uk>
2020-10-25 04:35:28 +0000

    Reduce the font size of the cta

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ import casestudiesData from '../data/casestudies'
 const howCanWeHelp = {
   name: 'How can we help',
   link: '/contect',
-  size: 'text-2xl'
+  size: 'text-1xl'
 
 }
 
@@ -19,7 +19,7 @@ function Hero () {
       <img src={randomCaseStudy.image} className='object-cover w-full h-200 h-600px' />
       <div className='absolute top-0 left-0 w-full h-600px bg-white bg-opacity-75' />
 
-      <div className='px-6 py-4 absolute left-0 bottom-150px w-3/4'>
+      <div className='mx-auto px-6 py-4 absolute left-0 bottom-150px w-3/4'>
         <h1 className='font-open-sans font-light text-5xl mb-2'>Latest Case Study: {randomCaseStudy.name}</h1>
         <p className='font-open-sans font-light text-black text-xl mb-4'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
