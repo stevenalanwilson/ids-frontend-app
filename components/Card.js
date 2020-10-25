e69b3b7266b051f9f470e13a73e0f1584b670454
Steven Wilson <steven@dominion79.co.uk>
2020-10-25 13:05:52 +0000

    Refactor Card comopnent to be generic

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,18 @@
 import Link from 'next/link'
 
 function Card ({ data }) {
-  console.log(data.fields.serviceGraphic.fields.title)
   return (
     <div className='card bg-white border-b-8 border-ids-orange'>
-      <Link href={data.fields.serviceName}>
-        <a href='{data.fields.serviceName}' title='{data.name}'>
-          <img src={data.fields.serviceGraphic.fields.file.url} alt={data.fields.serviceGraphic.fields.title} />
+      <Link href={data.fields.name}>
+        <a href='{data.fields.serviceName}' title='{data.fields.serviceName}'>
+          <img src={data.fields.graphic.fields.file.url} alt={data.fields.graphic.fields.title} />
         </a>
       </Link>
       <div className='details min-h-85px'>
         <p className='font-open-sans font-light text-black p-4  text-lg border-'>
-          <Link href={data.fields.serviceName}>
-            <a href='{data.fields.serviceName}' title='{data.fields.serviceName}'>
-              {data.fields.serviceName}
+          <Link href={data.fields.name}>
+            <a href='{data.fields.name}' title='{data.fields.name}'>
+              {data.fields.name}
             </a>
           </Link>
         </p>
