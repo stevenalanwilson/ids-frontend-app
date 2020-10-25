f5edada129c36a2e2c44b4832ba506410585e3ba
Steven Wilson <steven@dominion79.co.uk>
2020-10-25 12:33:33 +0000

    Add icons to menu

--- components/MenuLink.js
+++ components/MenuLink.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBuilding, faFileAlt, faDraftingCompass } from '@fortawesome/free-solid-svg-icons'
 
 const transistionStyles = [
   'transition',
@@ -19,14 +21,26 @@ const buttonStyles = [
   'py-1',
   'px-3',
   'font-open-sans',
-  'font-normal'
+  'font-light'
 ]
+/* eslint-disable */
+const map = {
+  'Services': faDraftingCompass, 
+  'Sectors': faBuilding, 
+  'Case Studies': faFileAlt
+}
+/* eslint-enable */
 
 function MenuLink ({ data }) {
   return (
     <li className='mr-6'>
       <Link href={data.link}>
-        <a href='#' className={buttonStyles.join(' ').toString()}>{data.name}</a>
+        <a href='#' className={buttonStyles.join(' ').toString()}>
+          {data.name}
+          <FontAwesomeIcon size='lg' icon={map[data.name]} className='ml-2' />
+
+        </a>
+
       </Link>
     </li>
   )
