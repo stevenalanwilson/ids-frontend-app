2793f207cb8016f08e191059db56c98e7a16b40b
Steven Wilson <steven@dominion79.co.uk>
2020-10-26 07:54:53 +0000

    Refactor Card component styles into objects

--- components/Card.js
+++ components/Card.js
@@ -6,27 +6,39 @@ const transistionStyles = [
   'ease-in-out'
 ]
 
-const ctaButtonStyles = [
-  ...transistionStyles,
-  'group',
-  'card',
-  'hover:bg-ids-orange',
-  'hover:-translate-y-1', 
-  'hover:scale-105',
-  'bg-white',
-  'border-b-8',
-  'border-ids-orange',
-  'transform'
-]
-
 function Card ({ data }) {
+
+  const ctaButtonStyles = {
+    outer: [
+      ...transistionStyles,
+      data.size,
+      'group',
+      'card',
+      'hover:bg-ids-orange',
+      'hover:-translate-y-1', 
+      'hover:scale-105',
+      'bg-white',
+      'border-b-8',
+      'border-ids-orange',
+      'transform'
+    ],
+    inner: [
+      'font-open-sans',
+      'font-light',
+      'text-black',
+      'group-hover:text-white',
+      'p-4',
+      'text-lg'
+    ]
+  }
+
   return (
-    <div className={data.size + ' ' + ctaButtonStyles.join(' ').toString()}>
+    <div className={ctaButtonStyles.outer.join(' ').toString()}>
       <Link href={data.fields?.name}>
         <a href={data.fields?.name} title={data.fields?.name}>
           <img src={data.fields.graphic.fields.file?.url} alt={data.fields.graphic.fields?.title} />
-          <div className='details min-h-85px'>
-            <p className='font-open-sans font-light text-black group-hover:text-white p-4  text-lg border-'>
+          <div className='min-h-85px'>
+            <p className={ctaButtonStyles.inner.join(' ').toString()}>
               {data.fields?.name}
             </p>
           </div>
