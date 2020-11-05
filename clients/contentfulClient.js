47560ae449af78a84a04d92edc586d780a523724
Steven Wilson <steven@dominion79.co.uk>
2020-11-05 20:58:38 +0000

    Refactored contnet clinet service into api

--- clients/contentfulClient.js
+++
@@ -1,8 +0,0 @@
-const contentfulClient = (config) => {
-  return require('contentful').createClient({
-    space: config.space,
-    accessToken: config.accessToken
-  })
-}
-
-module.exports = contentfulClient
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,12 @@ import Card from '../components/Card'
 
 import casestudyData from '../data/casestudies'
 
-import contentfulClient from '../clients/contentfulClient'
+import { getAllServices, getAllSectors, getAllClients } from '../lib/api'
 
 const randomTestimonial = casestudies => casestudies[Math.floor(Math.random() * (casestudies.length - 1))]
 const testimonial = randomTestimonial(casestudyData)
 
-const index = props => {
-
+export default function Index({ allServices, allSectors, allClients }) {
 
   return (
     <>
@@ -18,7 +17,7 @@ const index = props => {
           <div className='w-full pt-4 px-10 pb-10'>
             <h2 className='text-5xl text-white font-open-sans font-light mb-4'>Services</h2>
             <ul className='flex flex-wrap list-none'>
-              {props.services.items.map(entry => {
+              {allServices.items.map(entry => {
                 return <li className='w-1/6 flex-none p-2'><Card data={entry} /></li>
               }
               )}
@@ -32,7 +31,7 @@ const index = props => {
           <div className='w-1/2 p-10'>
             <h2 className='text-5xl text-white font-open-sans font-light mb-4'>Sectors</h2>
             <ul className='flex flex-wrap'>
-              {props.sectors.items.map(entry => {
+              {allSectors.items.map(entry => {
                 return <li className='w-1/3 flex-none p-2'><Card data={entry} /></li>
               }
               )}
@@ -52,7 +51,7 @@ const index = props => {
           <div className='w-full p-10 ml-5 '>
             <h2 className='text-5xl text-black font-open-sans font-light mb-4'>Who we work with</h2>
             <ul className='flex flex-wrap list-none'>
-              {props.clients.items.map(entry => {
+              {allClients.items.map(entry => {
                 return <li className='w-1/6 flex-none p-2'><img src={entry.fields.logo.fields.file.url} alt={entry.fields.logo.fields.title} /></li>
               }
               )}
@@ -65,27 +64,12 @@ const index = props => {
   )
 }
 
-index.getInitialProps = async () => {
 
-  const client = contentfulClient()
-
-  const clients = await client.getEntries({
-    content_type: 'client'
-  })
-
-  const services = await client.getEntries({
-    content_type: 'service'
-  })
-
-  const sectors = await client.getEntries({
-    content_type: 'sector'
-  })
-
+export async function getStaticProps() {
+  const allServices = await getAllServices()
+  const allSectors = await getAllSectors()
+  const allClients = await getAllClients()
   return {
-    services: services,
-    sectors: sectors,
-    clients: clients
+    props: { allServices, allSectors, allClients },
   }
 }
-
-export default index
