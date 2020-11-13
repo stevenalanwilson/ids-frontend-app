3f8fe09d37d49b4973765c70362eccaa01a990a2
Steven Wilson <steven@dominion79.co.uk>
2020-11-13 17:27:00 +0000

    Refactot services
    - Remove layout
    - make dynamic

--- pages/services.js
+++ pages/services.js
@@ -1,12 +1,39 @@
 import Link from 'next/link'
 import Card from '../components/Card'
+import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import PageHeader from '../components/PageHeader'
+import useSticky from '../hooks/useSticky.js'
 
-import { getAllServices, getAllSectors, getAllClients } from '../lib/api'
 
-export default function Index({ allServices, allSectors, allClients }) {
 
+import { getAllServices, getAServiceByID } from '../lib/api'
+
+function Index({ allServices, service}) {
+  const { isSticky, element } = useSticky()
   return (
     <>
+    <div className='shadow-md mx-auto'>
+      <Head>
+        <title>IDS Website - Prototype</title>
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+
+      <main className='relative w-full'>
+        <div>
+          <div>
+            <Header sticky={isSticky} />
+            <PageHeader element={element} />
+          </div>
+        </div>
       <div className='pb-4 bg-ids-dark-green'>
         <div className='container mx-auto '>
           <div className='w-full pt-4 px-10 pb-10'>
@@ -33,16 +60,28 @@ export default function Index({ allServices, allSectors, allClients }) {
           </div>
         </div>
       </div>
+      <div className='bg-black'>
+          <div className='container mx-auto '>
+            <div className='w-full'>
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
     </>
   )
 }
 
-
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  console.log(context.query.id)
   const allServices = await getAllServices()
-  const allSectors = await getAllSectors()
-  const allClients = await getAllClients()
+  const service = await getAServiceByID(context.query.id)
+  console.log(service)
   return {
-    props: { allServices, allSectors, allClients },
+    props: { allServices, service },
   }
+
 }
+
+export default Index
