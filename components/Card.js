0741be159cbc8450ce6901151ff7a52fc74fd574
Steven Wilson <steven@dominion79.co.uk>
2021-06-14 20:44:42 +0100

    Update card to use cached images

--- components/Card.js
+++ components/Card.js
@@ -25,19 +25,23 @@ function Card ({ data, path }) {
 
   return (
     <div className={data.size + ' ' + ctaButtonStyles.join(' ').toString()}>
-      <Link href={{
+      {/* <Link href={{
               pathname: path,
               query: { id: data.sys.id },
             }}>
-        <a>
-          <img src={graphic} alt={data.fields?.name} />
+        <a> */}
+          <picture>
+            <source media='(min-width: 800px)' srcset={graphic + '?fit=thumb&w=800&h=600&fm=webp&q=80'} />
+            <source media='(min-width: 400px)' srcset={graphic + '?fit=thumb&w=400&h=300&fm=webp&q=80'} />
+            <img src={graphic + '?fit=thumb&w=400&h=300&fm=webp&q=80'} alt='{data.fields?.name}' />
+          </picture>
           <div className='details min-h-85px'>
             <p className='font-open-sans font-light text-black group-hover:text-white p-4  text-lg border-'>
               {data.fields?.name}
             </p>
           </div>
-        </a>
-      </Link>
+        {/* </a>
+      </Link> */}
     </div>
   )
 }
